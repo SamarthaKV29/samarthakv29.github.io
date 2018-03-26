babe46ede05b64464793b5fc59a6115b7799435a
Samartha K V <samarthakv29@gmail.com>
2018-03-26 13:54:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-header( 'Location: /index.html' ) ;  ?>
+$ch = curl_init('https://urlxray.expeditedaddons.com/?api_key=20NPDES854TL7B9MOH5RYQW730FV816U6G1XAC4ZJI92K3&url=http://www.wikipedia.org&fetch_content=false');
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
+header( 'Location: /index.html' ) ;  
+
+
+?>
 
